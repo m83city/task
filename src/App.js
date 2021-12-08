29ff24d83f8bd9dd640c45bfe53345d093eeb786
Dima <rostorguievv@gmail.com>
2021-12-08 23:58:30 +0200

    delete stroke

--- src/App.js
+++ src/App.js
@@ -7,44 +7,47 @@ import { useState } from 'react';
 import Popup from './components/Popup'
 
 
-function App() {
- let tableId = 0 ;
+function App() { //start Array
     const [tableRows, setTableRows] = useState([
       {
         id: Date.now(),
         firstName: "lorem",
         secondName: "lorem",
-        lastName: "lorem"}
+        lastName: "lorem"},
   ]);
 
   const togglePopup = () =>{
     alert("ff")
   }
   
-  
-  
-    const addNewRow = () =>{
+    const addNewRow = () =>{ // add new Row
       const newRow = {
         id:Date.now(),
-        firstName: "",
-        secondName: "",
-        lastName: ""
+        firstName: "fff",
+        secondName: "fff",
+        lastName: "fff"
       } 
       setTableRows([...tableRows, newRow])
+    }
+
+    
+    function deleteRow (id){ //delete Row
+      setTableRows(tableRows.filter((rowTable) => rowTable.id !== id))
     }
 
   return (
     <div className="App">
       <input value = "Popup" type = "button" onClick = {togglePopup}/>
-      <Button onClick = {addNewRow}>Add new stroke</Button>
+      <Button onClick ={ addNewRow}  >Add new stroke</Button>
       <ul className = 'ul__box'>
         {tableRows.map((row,id) =>{
           if(row)
-          return(<Li row={row} key={id}  />  )
+          return(<Li row={row} key={id}  onChange = {deleteRow}/>  )
         }
         )}
          
       </ul>
+      
     </div>
   );
 }
--- src/components/Li.js
+++ src/components/Li.js
@@ -3,21 +3,12 @@ import Inputs from './Inputs';
 import {Button} from '../../node_modules/primereact/button'
 import '../component_style/Li.css'
 
-function Li(props, deleteRows){
-    let idTable = props.row.id
-    const showProps = () =>{
-        console.log(props.row.id)
-    }
-    ///////////////////
-    const removeRow = () =>{
-        console.log(idTable) // id Element
-        let a = deleteRows.deleteRow(idTable) // non-work code
-    }
-    ///////////////////
+function Li(props, onChange){
+    
     return(
         <li className = "item_li">
             <Inputs valTable = {props}/>
-            <Button  onClick = {removeRow}>Delete</Button>
+            <input type = "button" onClick = {() => props.onChange(props.row.id)} value = "Delete" />
         </li>
         
     )
