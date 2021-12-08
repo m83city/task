352826d7c8189dd1c68f5d81091c30df8ac55eb1
Dima <rostorguievv@gmail.com>
2021-12-08 23:29:48 +0200

    add new stroke v1.1

--- src/components/Li.js
+++ src/components/Li.js
@@ -2,24 +2,23 @@ import React from 'react';
 import Inputs from './Inputs';
 import {Button} from '../../node_modules/primereact/button'
 import '../component_style/Li.css'
+import PropTypes from 'prop-types'
 
-function Li(props, deleteRows){
-    let idTable = props.row.id
+function Li(props, onChange ){
+    
     const showProps = () =>{
         console.log(props.row.id)
     }
-    ///////////////////
-    const removeRow = () =>{
-        console.log(idTable) // id Element
-        let a = deleteRows.deleteRow(idTable) // non-work code
-    }
+    ///////////////////onDelete(props.row.id)
+    
     ///////////////////
     return(
         <li className = "item_li">
             <Inputs valTable = {props}/>
-            <Button  onClick = {removeRow}>Delete</Button>
+            
         </li>
         
     )
 }
+
 export default  Li;
